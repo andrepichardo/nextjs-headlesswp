e587e2152f6c521acff725b7d922d4e050bce6ab
André Pichardo <andrep97@hotmail.com>
2025-02-11 14:45:31 -0400

    fix: feature image condition

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,5 @@
-import { getPostBySlug } from '@/lib/api';
-import Image from 'next/image';
+import { getPostBySlug } from "@/lib/api";
+import Image from "next/image";
 
 export default async function Post({
   params,
@@ -18,12 +18,14 @@ export default async function Post({
     <article className="py-8 flex flex-col gap-6 items-center">
       <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
       <div className="flex flex-col xl:flex-row gap-8 items-center">
-        <Image
-          src={post.featuredImage.node.sourceUrl}
-          alt={post.title}
-          width={800}
-          height={400}
-        />
+        {post.featuredImage && (
+          <Image
+            src={post.featuredImage.node.sourceUrl}
+            alt={post.title}
+            width={800}
+            height={400}
+          />
+        )}
         <div
           className="prose lg:prose-xl flex flex-col gap-3 text-justify"
           // biome-ignore lint/security/noDangerouslySetInnerHtml: Es necesario para renderizar el contenido HTML
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
-import { GraphQLClient } from 'graphql-request';
+import { GraphQLClient } from "graphql-request";
 
 const endpoint =
   process.env.NEXT_PUBLIC_WORDPRESS_API_URL ||
-  'http://headlesswp.local/graphql';
+  "http://headlesswp.local/graphql";
 
 const graphQLClient = new GraphQLClient(endpoint);
 
